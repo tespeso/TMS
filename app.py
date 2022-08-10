6a17ae4862149b668dd3d08a7b1fa5c283c16d59
tespeso <tespeso@free.fr>
2022-08-10 13:49:27 +0200

    Base app

--- app.py
+++ app.py
@@ -1,2 +1,19 @@
+import sys
+from PySide6.QtCore import Qt, QSize
+from PySide6.QtWidgets import QApplication, QMainWindow
 
-print("hello GH")
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("TMS")
+        self.setFixedSize(QSize(1000, 700))
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
